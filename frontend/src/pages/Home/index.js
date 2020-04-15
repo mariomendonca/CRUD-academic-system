6a53efed8b8d75ae14f515197537fab14f5c0ea5
Mario Mendonca <mariomeendonca00@gmail.com>
2020-04-15 14:49:24 -0300

    front

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -5,7 +5,20 @@ import './styles.css'
 export default function Home() {
     return (
         <div className="home-container">
-            <h1>netozera</h1>
+          <div className='header'>
+          
+            <h2>Academic System</h2>
+            <h4>by: Mario Mendonca, using React and nodejs</h4>
+
+          </div>
+          <div className="content">
+         
+              <button className='button'>Create</button>
+              <button className='button'>List</button>
+              <button className='button'>Update</button>
+              <button className='button'>Delete</button>
+           
+          </div>
         </div>
     )
 }
