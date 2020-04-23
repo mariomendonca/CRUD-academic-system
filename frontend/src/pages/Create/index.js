48a0996e78bfa8c0de393080009a31a3a52a9169
Mario Mendonca <mariomeendonca00@gmail.com>
2020-04-23 17:10:14 -0300

    finalizando register page

--- frontend/src/pages/Create/index.js
+++ frontend/src/pages/Create/index.js
@@ -1,9 +1,39 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import { FiArrowLeft } from 'react-icons/fi'
+import api from '../../services/api'
 
 import './styles.css'
+
 export default function Create() {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [cpf, setCpf] = useState('')
+    const [nationality, setNationality] = useState('')
+    const [numberPhone, setNumberPhone] = useState('')
+    
+    //responsavel por fazer o cadastro do usuario
+    async function handleRegister(event){ 
+        event.preventDefault() //prevenir o comportamento padrao (spa)
+        
+        const data = {
+            name,
+            email,
+            cpf,
+            nationality,
+            numberPhone
+        }
+
+         try {
+            //enviando para o database
+            await api.post('students', data)
+            alert('user sucessfully registered')
+            console.log('foi')
+        } catch (err) {
+            alert('something went wrong, try again')
+        }
+    }
+
     return(
         <div className="create-container">
             <div className='header'>
@@ -22,12 +52,33 @@ export default function Create() {
                         back to menu
                 </Link>
             </section>
-            <form>
-                <input placeholder="name"/>
-                <input type='email' placeholder="E-mail"/>
-                <input placeholder="Cpf"/>
-                <input placeholder="Nationality"/>
-                <input placeholder="Number Phone"/>
+            <form onSubmit={handleRegister}>
+                <input 
+                  placeholder="name"
+                  value={name}
+                  onChange={event => setName(event.target.value)}
+                />
+                <input 
+                  type='email' 
+                  placeholder="E-mail"
+                  value={email}
+                  onChange={event => setEmail(event.target.value)}
+                />
+                <input 
+                  placeholder="Cpf"
+                  value={cpf}
+                  onChange={event => setCpf(event.target.value)}
+                />
+                <input 
+                  placeholder="Nationality"
+                  value={ nationality }
+                  onChange={ event => setNationality(event.target.value)}
+                />
+                <input 
+                  placeholder="Number Phone"
+                  value={ numberPhone }
+                  onChange={ event => setNumberPhone(event.target.value)}
+                />
                 <button className='RegisterBtn' type='submit'>Register</button>
             </form>
             </div>
